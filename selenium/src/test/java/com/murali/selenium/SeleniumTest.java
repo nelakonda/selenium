053c090016e4a6f9fd4ad75463b66b5f73739037
nelakonda <nelakonda@gmail.com>
2017-04-07 10:56:24 -0500

    Add files via upload

---
+++ selenium/src/test/java/com/murali/selenium/SeleniumTest.java
@@ -0,0 +1,16 @@
+package com.murali.selenium;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class SeleniumTest  {
+	
+	@Test
+    public  void testRun() {
+    	SeleniumTestRunner runner = new SeleniumTestRunner();
+    	Assert.assertTrue(runner.run());
+    	
+    }
+}
