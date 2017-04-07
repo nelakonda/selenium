190590a095711c55d962b53505712cb5e339ff59
muraleedharnelakonda <muraleedharnelakonda@muraleedharsmbp.attlocal.net>
2017-04-07 11:04:47 -0500

    Another Test

--- selenium/src/test/java/com/murali/selenium/SeleniumTest.java
+++ selenium/src/test/java/com/murali/selenium/SeleniumTest.java
@@ -4,13 +4,17 @@ package com.murali.selenium;
 import org.junit.Assert;
 import org.junit.Test;
 
-
+/**
+ * Simple Test class 
+ * @author muraleedharnelakonda
+ *
+ */
 public class SeleniumTest  {
 	
 	@Test
     public  void testRun() {
     	SeleniumTestRunner runner = new SeleniumTestRunner();
-    	Assert.assertFalse(runner.run());
+    	Assert.assertTrue(runner.run());
     	
     }
 }
